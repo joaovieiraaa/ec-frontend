93217379a78a7b1a220f1cbaa608159eb5515514
Joao Vieira <joao.vieira@jimpisoft.com>
2024-04-12 11:06:49 +0100

    commit

--- src/components/footers/footer-one/footer-one.vue
+++ src/components/footers/footer-one/footer-one.vue
@@ -1,2 +1,8 @@
 <template src="./footer-one.html"></template>
 <style lang="scss" src="./footer-one.scss"></style>
+
+<script setup lang="ts">
+import { currentYear } from "@/utils/date";
+
+const year = currentYear();
+</script>
